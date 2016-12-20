c365a65e4fcae214dad2895a9234b65069b52731
benjie estrella <bench323010@gmail.com>
2016-12-20 21:21:12 +0800

    set Signed-off-by: benjie estrella <bench323010@gmail.com>

---
+++ webtaskManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webtaskManagement.Startup))]
+namespace webtaskManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
